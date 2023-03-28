3c30963ededba4d074851c42117b4794ca3b37ad
Abdel7004 <abdelrahmanh15@gmail.com>
2023-03-28 07:59:27 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+// require packages for Express
+
+const { name } = require('ejs');
+const express = require('express');
+
+const app = express();
+
+
+//set port to a variable
+
+const port = 3000;
+
+//Exported drinks data 
+
+const budgets = require('./models/budgets.js');
+
+//Middleware
+
+app.use(express.urlencoded({extended: false}));
+
+//Root Route 
+
+app.get('/', (req, res) => {
+    res.send(budgets);
+});
+
+// Index Route
+app.get('/budgets/', (req, res) => {
+    res.render(
+        'index.ejs',
+        {
+            allBudgets:budgets
+        }
+    );
+  });
+
+// NEW ROUTE
+app.get('/budgets/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+// CREATE ROUTE
+  app.post('/budgets', (req, res)=>{
+    budgets.push(req.body);
+    res.redirect('/budgets');
+  });
+
+
+   // SHOW ROUTE 
+   
+  app.get('/budgets:indexOfBudgetsArray', (req, res)=>{
+    res.render('show.ejs', {
+      budgets: budgets[req.params.indexOfBudgetsArray]
+    });
+  });
+
+app.listen(3000, () =>{
+  console.log('listening');
+});
